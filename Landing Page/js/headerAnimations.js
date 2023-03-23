197670c9e33f071c2dceadb8f11a44b563c7275e
jacob <blxckkokoro@gmail.com>
2023-03-23 11:58:23 +0100

    Added animations for header

--- Landing Page/js/headerAnimations.js
+++ Landing Page/js/headerAnimations.js
@@ -1,14 +1,52 @@
+const header = document.getElementsByTagName("header")[0];
+
 const headerInfoDisplay = document.querySelector(".header__info--display");
 
-window.addEventListener(
-  "load",
-  () => {
-    headerInfoDisplay.classList.add("header__info--display--show");
+const navButtons = document.getElementsByClassName("navButtons")[0];
 
+function showHeaderBackground() {
+  return new Promise((resolve, reject) => {
+    header.childNodes[0].classList.add("header__background--show");
+    resolve();
+  });
+}
+
+function showHeaderInfoDisplay() {
+  return new Promise((resolve, reject) => {
+    headerInfoDisplay.classList.add("header__info--display--show");
+    resolve();
+  });
+}
+
+function showHeaderInfoDisplayContent() {
+  return new Promise((resolve, reject) => {
     headerInfoDisplay.childNodes.forEach((element) => {
-      console.log(element);
-      element.classList.add("show-header-info");
+      element.classList.add("header__info--display--content--show");
+      resolve();
     });
-  },
-  false
-);
+  });
+}
+
+function showNavButtons() {
+  return new Promise((resolve, reject) => {
+    let delay = 0;
+    navButtons.childNodes.forEach((element) => {
+      setTimeout(() => {
+        element.classList.add("navButtons--show");
+      }, delay);
+      delay += 200;
+    });
+    resolve();
+  });
+}
+
+async function loadHeader() {
+  await showHeaderBackground();
+  await showHeaderInfoDisplay();
+  await showHeaderInfoDisplayContent();
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await showNavButtons();
+}
+
+window.addEventListener("load", loadHeader, false);
